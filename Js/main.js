ae4b45ea1881a050170302ac1df4ec04db0142f2
sana <sananafari1382@gmail.com>
2024-09-23 01:00:57 +0330

    finished version

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,38 @@
-const countdown = document.querySelector('.countdown');
+const countdown = document.querySelector('.countdown');  
 
-//Set Launch Date
-const 
+// Set Launch Date (ms) - Change to a future date  
+const launchDate = new Date('Jan 1, 2025 13:00:00').getTime();  // Set a future date  
+
+// Update every second  
+const intvl = setInterval(() => {  
+  // Get today's date and time (ms)  
+  const now = new Date().getTime();  
+
+  // Distance from now and the launch date (ms)  
+  const distance = launchDate - now;  
+
+  // Time calculation  
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));  
+  const hours = Math.floor(  
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)  
+  );  
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));  
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);  
+
+  // Display result  
+  countdown.innerHTML = `  
+  <div>${days}<span>Days</span></div>   
+  <div>${hours}<span>Hours</span></div>  
+  <div>${mins}<span>Minutes</span></div>  
+  <div>${seconds}<span>Seconds</span></div>  
+  `;  
+
+  // If launch date is reached  
+  if (distance < 0) {  
+    // Stop countdown  
+    clearInterval(intvl);  
+    // Style and output text  
+    countdown.style.color = '#17a2b8';  
+    countdown.innerHTML = 'Launched!';  
+  }  
+}, 1000);  
